2a397c9bc7df08329ff43e75f6418f7bf3a54c28
Ardihyundai <primaak1990@gmail.com>
2026-02-18 00:40:29 +0700

    Update global.js

--- global.js
+++ global.js
@@ -225,5 +225,26 @@ window.addEventListener("scroll", ()=>{
         if(rect.top < window.innerHeight - 100){
             card.classList.add("show");
         }
+
+       // ACCORDION LINEUP
+document.addEventListener("DOMContentLoaded", function(){
+
+  const accButtons = document.querySelectorAll(".accordion-btn");
+
+  accButtons.forEach(button => {
+    button.addEventListener("click", function(){
+
+      const content = this.nextElementSibling;
+
+      if(content.style.display === "block"){
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+
+    });
+  });
+
+});
     });
 });
